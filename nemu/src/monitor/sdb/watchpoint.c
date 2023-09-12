a84f42ebe5c557a62bc460c36812810b510b29a4
tracer-ics2023 <tracer@njuics.org>
2023-09-12 11:48:33 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-79-generic #86-Ubuntu SMP Mon Jul 10 16:07:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:48:33 up 13 days, 16:54,  4 users,  load average: 1.05, 0.94, 0.79

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -22,7 +22,8 @@ typedef struct watchpoint {
   struct watchpoint *next;
 
   /* TODO: Add more members if necessary */
-
+  char* expression;
+  word_t value;
 } WP;
 
 static WP wp_pool[NR_WP] = {};
@@ -33,6 +34,7 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    wp_pool[i].expression = NULL;
   }
 
   head = NULL;
@@ -41,3 +43,88 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+static WP* new_wp() {
+  if (free_ == NULL) {
+    panic("Watchpoint pool is full.");
+  }
+
+  WP* ret = free_;
+  ret->next = head;
+  head = ret;
+  free_ = free_->next;
+  return ret;
+}
+
+static bool free_wp(WP *wp) {
+  if (head == NULL || wp == NULL) {
+    goto error;
+  }
+  if (head == wp) {
+    WP* tmp = head->next;
+    head->next = free_;
+    free_ = head;
+    head = tmp;
+    return true;
+  }
+  for (WP *cur = head; cur != NULL; cur = cur->next) {
+    if (cur->next == wp) {
+      WP* tmp = cur->next;
+      cur->next = tmp->next;
+      tmp->next = free_;
+      free_ = tmp;
+      break;
+    }
+  }
+error: // fall through
+  return false;
+}
+
+bool watchpoint_scan() {
+  bool hit = 0;
+
+  for (WP *cur = head; cur != NULL; cur = cur->next) {
+    bool success = 0;
+    word_t nvalue = expr(cur->expression, &success);
+    if (nvalue != cur->value) {
+      hit = 1;
+      cur->value = nvalue;
+      break;
+    }
+  }
+
+  return hit;
+}
+
+int watchpoint_add(char *expression) {
+  bool success = 0;
+  word_t value = expr(expression, &success);
+  if (!success) goto error;
+  WP *wp = new_wp();
+  wp->expression = strdup(expression);
+  wp->value = value;
+  return wp->NO;
+error:
+  Info("Invalid EXPR: %s", expression);
+  return -1;
+}
+
+void watchpoint_remove(int index) {
+  if (index < 0 || index >= NR_WP) goto error;
+
+  if (wp_pool[index].expression != NULL) {
+    free(wp_pool[index].expression);
+  }
+  if (!free_wp(&wp_pool[index])) goto error;
+
+  return;
+
+error:
+  Error("No such watchpoint: %d", index);
+}
+
+void watchpoint_list() {
+  Info("Id    Value         Expression");
+  for (WP* cur = head; cur != NULL; cur = cur->next) {
+    printf("%4d  %12u  %s\n", cur->NO, cur->value, cur->expression);
+  }
+}
