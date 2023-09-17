5e12b18b7f25d91cd673dc2ab52405145ffb946d
tracer-ics2023 <tracer@njuics.org>
2023-09-17 19:51:17 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:51:17 up 4 days, 17 min,  6 users,  load average: 1.32, 0.91, 0.79

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -179,7 +179,7 @@ static int decode_exec(Decode *s) {
     #ifdef CONFIG_FTRACE
     if (FTRACE_COND) {
       if (BITS(s->isa.inst.val, 11, 7) == 1) {
-        fprintf(stderr, "jal called and rd is x1.\n");
+        // fprintf(stderr, "jal called and rd is x1.\n");
         ftrace_call(s->pc, s->dnpc);
       }
     }
@@ -191,10 +191,10 @@ static int decode_exec(Decode *s) {
     #ifdef CONFIG_FTRACE
     if (FTRACE_COND) {
       if (BITS(s->isa.inst.val, 19, 15) == 1) {
-        fprintf(stderr, "jalr called and rs1 is x1.\n");
+        // fprintf(stderr, "jalr called and rs1 is x1.\n");
         ftrace_ret(s->pc, s->dnpc);
       } else if (BITS(s->isa.inst.val, 11, 7) == 1) {
-        fprintf(stderr, "jalr called and rd is x1.\n");
+        // fprintf(stderr, "jalr called and rd is x1.\n");
         ftrace_call(s->pc, s->dnpc);
       }
     }
