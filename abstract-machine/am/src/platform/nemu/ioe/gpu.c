93e5771e4e0086a75a9ecaba7b8e7f14f814b138
tracer-ics2023 <tracer@njuics.org>
2023-09-19 11:17:38 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:17:38 up 5 days, 15:43,  5 users,  load average: 0.62, 0.65, 0.68

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,13 +2,18 @@
 #include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
+#include <stdint.h>
+#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int w, h;
+
 void __am_gpu_init() {
   int i;
-  int w = inl(VGACTL_ADDR) >> 16;
-  int h = inl(VGACTL_ADDR) & 0xff;
+  w = inl(VGACTL_ADDR) >> 16;
+  h = inl(VGACTL_ADDR) & 0xff;
+  printf("AM GPU initialzed, width: %d, height: %d.\n", w, h);
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -23,6 +28,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  for (int i = 0; i < ctl->w; i ++) {
+    for (int j = 0; j < ctl->h; j ++) {
+      outl(FB_ADDR + (ctl->x + i) * w + (ctl->y + j), *((uint32_t *) ctl->pixels + ctl->w * i + j));
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
