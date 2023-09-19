880d95d0f208f9858245b3fe48cefab419ec8181
tracer-ics2023 <tracer@njuics.org>
2023-09-19 10:03:35 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:03:35 up 5 days, 14:29,  5 users,  load average: 0.94, 1.22, 1.46

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -1,6 +1,8 @@
 #ifndef __AMDEV_H__
 #define __AMDEV_H__
 
+#include <am.h>
+
 // **MAY SUBJECT TO CHANGE IN THE FUTURE**
 
 #define AM_DEVREG(id, reg, perm, ...) \
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,13 @@
+#include "amdev.h"
+#include "riscv/riscv.h"
 #include <am.h>
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int k = inl(AM_INPUT_KEYBRD);
+
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
