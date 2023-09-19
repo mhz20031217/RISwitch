70056c108dabac664b55ffd33a97043489ba9300
tracer-ics2023 <tracer@njuics.org>
2023-09-19 08:53:19 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:53:19 up 5 days, 13:19,  4 users,  load average: 0.31, 0.46, 0.53

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,20 @@
 #include <am.h>
+#include <amdev.h>
 #include <nemu.h>
 
+static uint64_t boot_time = 0;
+
 void __am_timer_init() {
+  boot_time = 0;
+  boot_time |= inl(AM_TIMER_UPTIME);
+  boot_time |= (uint64_t) inl(AM_TIMER_UPTIME + 4) << 32;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint64_t cur_time = 0;
+  cur_time |= inl(AM_TIMER_UPTIME);
+  cur_time |= (uint64_t) inl(AM_TIMER_UPTIME + 4) << 32;
+  uptime->us = cur_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
