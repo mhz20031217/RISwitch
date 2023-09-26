3fb80367d3e7ac1562c3c5d5e70cade03a03f14e
tracer-ics2023 <tracer@njuics.org>
2023-09-26 09:02:41 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:02:41 up 3 days, 11:41,  3 users,  load average: 0.61, 0.97, 0.95

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -44,12 +44,9 @@ static void audio_fill_buffer(void *data, Uint8 *stream, int len) {
   int sum = 0;
 
   while (sum < len) {
-    while (audio_base[reg_lock] || audio_base[reg_count] == 0) {
-      usleep(10);
-    }
+    while (audio_base[reg_lock] || audio_base[reg_count] == 0) {}
     audio_base[reg_lock] = 1;
     // Info("Acquired lock.");
-    usleep(10);
 
     int count = audio_base[reg_count];
 
