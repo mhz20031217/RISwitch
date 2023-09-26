98cd73159793bffcc805969060cc3b6bf81a2279
tracer-ics2023 <tracer@njuics.org>
2023-09-26 08:57:01 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-82-generic #91-Ubuntu SMP Mon Aug 14 14:14:14 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:57:01 up 3 days, 11:35,  3 users,  load average: 1.31, 1.21, 0.95

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -82,7 +82,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
         .channels = audio_base[reg_channels],
         .silence = 0,
         .format = AUDIO_S16SYS,
-        .samples = audio_base[reg_samples],
+        .samples = 1024,
         .userdata = NULL,
         .callback = audio_fill_buffer
     };
