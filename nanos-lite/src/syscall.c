5f822121a103e59470a73855fcf70487a7cf5cb6
tracer-ics2023 <tracer@njuics.org>
2023-10-05 18:01:02 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:01:02 up 4 days, 13 min,  1 user,  load average: 1.20, 1.43, 1.54

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,22 +2,63 @@
 #include <sys/types.h>
 #include "syscall.h"
 
-int SYS_exit(uintptr_t args[]) {
+static inline int SYS_exit(uintptr_t args[]) {
   halt(args[0]);
   return 0;
 }
 
-int SYS_yield(uintptr_t args[]) {
+static inline int SYS_yield(uintptr_t args[]) {
   yield();
   return 0;
 }
+
+static inline int SYS_write(uintptr_t args[]) {
+  int fd = args[0];
+  const void *buf = (const void *) args[1];
+  size_t count = args[2];
+
+  if (fd == 1 || fd == 2) {
+    for (size_t i = 0; i < count; i ++) {
+      putch(*((const char *)(buf) + i));
+    }
+    return count;
+  } else {
+    return -1;
+  }
+}
+
+// enum {
+//   SYS_exit,    0
+//   SYS_yield,   1
+//   SYS_open,    2
+//   SYS_read,    3
+//   SYS_write,   4
+//   SYS_kill,    5
+//   SYS_getpid,  6
+//   SYS_close,   7
+//   SYS_lseek,   8
+//   SYS_brk,     9
+//   SYS_fstat,   10
+//   SYS_time,    11
+//   SYS_signal,  12
+//   SYS_execve,  13
+//   SYS_fork,    14
+//   SYS_link,    15
+//   SYS_unlink,  16
+//   SYS_wait,    17
+//   SYS_times,   18
+//   SYS_gettimeofday 19
+// };
 
 static struct {
   int (*handler)(uintptr_t args[]);
   const char *desc;
 } syscall_handler[] = {
-  {SYS_exit, "exit"},
-  {SYS_yield, "yield"}
+  {SYS_exit, "exit"},     // 0
+  {SYS_yield, "yield"},
+  {NULL, NULL},
+  {NULL, NULL},
+  {SYS_write, "write"},   // 4
 };
 
 #define NR_SYSCALL ARRLEN(syscall_handler)
