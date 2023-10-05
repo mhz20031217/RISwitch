70d4dc8ace70c4d19bc2582cd9282c835337e523
tracer-ics2023 <tracer@njuics.org>
2023-10-05 19:24:40 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:24:40 up 4 days,  1:36,  1 user,  load average: 0.87, 1.33, 1.50

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -66,6 +66,9 @@ static struct {
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   if (a[0] >= NR_SYSCALL) {
     panic("Unhandled syscall ID = %d", a[0]);
