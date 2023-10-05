4aba5b89ff79e826c6eaaacb43ac0ffd7a8f027e
tracer-ics2023 <tracer@njuics.org>
2023-10-05 23:36:57 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:36:57 up 4 days,  5:48,  1 user,  load average: 1.45, 0.99, 0.80

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -30,6 +30,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  assert(filename);
   int fd = fs_open(filename, 0, 0);
   int rc = 0;
   Elf_Ehdr elf_header;
