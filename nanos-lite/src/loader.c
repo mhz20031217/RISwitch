cfd20dc584e338bc8e4faeed4b14994793296f43
tracer-ics2023 <tracer@njuics.org>
2023-10-05 11:39:56 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:39:56 up 3 days, 17:51,  3 users,  load average: 0.72, 1.28, 1.34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <debug.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,8 +10,50 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+// ISA-depedent definitions
+#if defined(__ISA_X86__)
+# define EXPECT_TYPE EM_386
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#elif defined(__riscv)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86_64__)
+# define EXPECT_TYPE EM_X86_64
+#else
+#error Unsupported ISA
+#endif
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len); 
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+  int rc = 0;
+  Elf_Ehdr elf_header;
+  rc = ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+  assert(rc == sizeof(Elf_Ehdr));
+
+  assert(*(uint32_t *)elf_header.e_ident == 0x7f454c46);
+  assert(elf_header.e_machine == EXPECT_TYPE);
+
+  uintptr_t phoff = elf_header.e_phoff;
+  uint32_t phnum = elf_header.e_phnum;
+  uint32_t phentsize = elf_header.e_phentsize;
+
+  Elf_Phdr entry;
+  for (uint32_t i = 0; i < phnum; i ++) {
+    uintptr_t entoff = phoff + i * phentsize;
+    rc = ramdisk_read(&entry, entoff, sizeof(Elf_Phdr));
+    assert(rc == sizeof(Elf_Phdr));
+
+    if (entry.p_type != PT_LOAD) continue;
+
+    rc = ramdisk_read((void *)entry.p_vaddr, entry.p_offset, entry.p_filesz);
+    assert(rc == entry.p_filesz);
+
+    memset((void *)(entry.p_vaddr + entry.p_filesz), 0, entry.p_memsz - entry.p_filesz);
+  }
+
   return 0;
 }
 
