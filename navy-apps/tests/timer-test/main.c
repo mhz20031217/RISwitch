8f153be95d35987194a19c12323a95c29a8ff918
tracer-ics2023 <tracer@njuics.org>
2023-10-06 10:25:58 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:25:58 up 4 days, 16:37,  1 user,  load average: 1.66, 1.06, 0.99

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -8,16 +8,17 @@ int main() {
   bool flag = 1;
   while (1) {
     gettimeofday(&tv, NULL);
+    printf("gettimeofday: %ld, %ld\n", tv.tv_sec, tv.tv_usec);
     if (flag) {
       if (tv.tv_usec < 5000) {
-        printf("%ld %ld", tv.tv_sec, tv.tv_usec);
+        printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
         flag = 0;
       }
     } else {
       if (tv.tv_usec < 5000) {
         continue;
       }
-      printf("%ld %ld", tv.tv_sec, tv.tv_usec);
+      printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
       flag = 1;
     }
   }
