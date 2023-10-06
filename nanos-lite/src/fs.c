4d486b5efb8c3a365a4fd6e6493923a06f53ce98
tracer-ics2023 <tracer@njuics.org>
2023-10-06 08:38:18 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:38:18 up 4 days, 14:50,  1 user,  load average: 0.94, 0.88, 0.76

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,10 +29,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
+static size_t serial_write(const void *buf, size_t offset, size_t len);
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
   [FD_FB] = {"fb", 0, 0, invalid_read, invalid_write},
 #include "files.h"
@@ -49,7 +51,7 @@ void init_fs() {
   // }
 }
 
-static inline size_t serial_write(const void *buf, size_t offset, size_t len) {
+static size_t serial_write(const void *buf, size_t offset, size_t len) {
   const char *src = buf;
 
   while (len --) {
