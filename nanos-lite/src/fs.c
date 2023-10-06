54c7191dcf16029155fe5af454cfc22a907a50d9
tracer-ics2023 <tracer@njuics.org>
2023-10-06 08:39:57 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:39:57 up 4 days, 14:51,  1 user,  load average: 0.90, 0.86, 0.77

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -35,7 +35,7 @@ static size_t serial_write(const void *buf, size_t offset, size_t len);
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
   [FD_FB] = {"fb", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
