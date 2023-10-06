ac4beb562a19ff59a137471afdb6d90272f00785
tracer-ics2023 <tracer@njuics.org>
2023-10-06 10:52:35 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:52:35 up 4 days, 17:04,  1 user,  load average: 0.61, 0.58, 0.69

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,23 +2,47 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <stdbool.h>
+#include <NDL.h>
+
+// int main() {
+//   struct timeval tv;
+//   bool flag = 1;
+//   while (1) {
+//     gettimeofday(&tv, NULL);
+//     // printf("gettimeofday: %ld, %ld\n", tv.tv_sec, tv.tv_usec);
+//     if (flag) {
+//       if (tv.tv_usec < 500000) {
+//         printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+//         flag = 0;
+//       }
+//     } else {
+//       if (tv.tv_usec < 500000) {
+//         continue;
+//       }
+//       printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+//       flag = 1;
+//     }
+//   }
+
+//   return 0;
+// }
 
 int main() {
-  struct timeval tv;
   bool flag = 1;
   while (1) {
-    gettimeofday(&tv, NULL);
+    uint32_t cur = NDL_GetTicks();
+    uint32_t msec = cur / 1000;
     // printf("gettimeofday: %ld, %ld\n", tv.tv_sec, tv.tv_usec);
     if (flag) {
-      if (tv.tv_usec < 500000) {
-        printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+      if (msec < 500) {
+        printf("%u\n", cur);
         flag = 0;
       }
     } else {
-      if (tv.tv_usec < 500000) {
+      if (msec < 500) {
         continue;
       }
-      printf("%ld %ld\n", tv.tv_sec, tv.tv_usec);
+      printf("%u\n", cur);
       flag = 1;
     }
   }
