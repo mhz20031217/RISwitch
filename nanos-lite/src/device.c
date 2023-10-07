b0955cda1e3f1a848f19839a4470e82e4b51e6ed
tracer-ics2023 <tracer@njuics.org>
2023-10-08 00:27:02 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-84-generic #93-Ubuntu SMP Tue Sep 5 17:16:10 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:27:02 up 6 days,  6:38,  2 users,  load average: 1.48, 0.92, 0.77

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -54,7 +54,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   io_write(AM_GPU_FBDRAW, 
     .pixels = (void *) buf,
     .w = len, .h = 1,
-    .y = offset / gpu_config.width, .x = offset % gpu_config.width,
+    .y = (offset >> 2) / gpu_config.width, .x = (offset >> 2) % gpu_config.width,
     .sync = true
   );
   return len;
