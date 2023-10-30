899bc46dcc4d856d2f19418ef8ff87342bff5505
tracer-ics2023 <tracer@njuics.org>
2023-10-30 11:26:07 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:26:07 up  1:04,  0 users,  load average: 1.38, 1.15, 1.05

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,4 +1,5 @@
 #include <common.h>
+#include <stddef.h>
 #include <stdio.h>
 
 #if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
@@ -18,8 +19,10 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t serial_write(const void *buf, size_t offset, size_t len) {
   const char *src = buf;
 
-  while (len --) {
+  size_t cnt = 0;
+  while (cnt < len) {
     putch(*(src ++));
+    cnt ++;
   }
 
   return len;
