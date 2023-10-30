e4ed2bed9902f97fe113bf3c9321254cd54443e9
tracer-ics2023 <tracer@njuics.org>
2023-10-30 23:33:25 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:33:25 up 13:11,  0 users,  load average: 1.71, 1.33, 1.12

--- navy-apps/tests/fixedptc-test/main.c
+++ navy-apps/tests/fixedptc-test/main.c
@@ -1,5 +1,11 @@
 #include <fixedptc.h>
+#include <stdio.h>
 
 int main() {
-    
+    printf(
+        "[libfixedptc] Test: 123.456 * 456.789 = %s.\n",
+        fixedpt_cstr(
+            fixedpt_mul(fixedpt_rconst(123.456), fixedpt_rconst(456.789)), 8));
+    printf("[libfixedptc] Test: floor(123.456) = %s.\n",
+      fixedpt_cstr(fixedpt_floor(fixedpt_rconst(123.456)), 8));
 }
