86ed9654134bfe337732eb10e9d6644ee6186d74
tracer-ics2023 <tracer@njuics.org>
2023-10-31 10:02:09 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:02:09 up 23:40,  1 user,  load average: 0.66, 0.59, 0.63

--- navy-apps/libs/libbmp/src/SDL_bmp.c
+++ navy-apps/libs/libbmp/src/SDL_bmp.c
@@ -1,10 +1,13 @@
 #include <SDL_bmp.h>
 #include <BMP.h>
 #include <assert.h>
+#include <stdio.h>
 
 SDL_Surface* SDL_LoadBMP(const char *filename) {
+  printf("[SDL_bmp] Loading '%s'.\n", filename);
   int w, h;
   void *pixels = BMP_Load(filename, &w, &h);
+  printf("[SDL_bmp] BMP_Load finished: %dx%d.\n", w, h);
   assert(pixels);
   SDL_Surface *s = SDL_CreateRGBSurfaceFrom(pixels, w, h, 32, w * sizeof(uint32_t),
       DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
