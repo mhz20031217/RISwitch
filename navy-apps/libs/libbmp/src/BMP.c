3fddff230b46e0a16863ff4d0e19a0e431045247
tracer-ics2023 <tracer@njuics.org>
2023-10-31 09:57:17 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:57:17 up 23:35,  1 user,  load average: 0.52, 0.52, 0.62

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -21,7 +21,10 @@ struct BitmapHeader {
 
 void* BMP_Load(const char *filename, int *width, int *height) {
   FILE *fp = fopen(filename, "r");
-  if (!fp) return NULL;
+  if (!fp) {
+    printf("[libbmp] cannot open file: '%s'.\n", filename);
+    return NULL;
+  }
 
   struct BitmapHeader hdr;
   assert(sizeof(hdr) == 54);
