f88ffc7feaaa13bc22dedecce3998fb9afbb058b
tracer-ics2023 <tracer@njuics.org>
2023-10-31 11:46:14 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:46:14 up 1 day,  1:24,  1 user,  load average: 0.80, 0.79, 1.03

--- navy-apps/libs/libbmp/src/SDL_bmp.c
+++ navy-apps/libs/libbmp/src/SDL_bmp.c
@@ -9,8 +9,9 @@ SDL_Surface* SDL_LoadBMP(const char *filename) {
   void *pixels = BMP_Load(filename, &w, &h);
   printf("[SDL_bmp] BMP_Load finished: %dx%d.\n", w, h);
   if (pixels == NULL) {
+    printf("[SDL_bmp] failed to load '%s'.\n", filename);
     volatile int i = 1;
-    while (1) printf("[SDL_bmp] failed to load '%s'.\n", filename);
+    while (i);
   }
   SDL_Surface *s = SDL_CreateRGBSurfaceFrom(pixels, w, h, 32, w * sizeof(uint32_t),
       DEFAULT_RMASK, DEFAULT_GMASK, DEFAULT_BMASK, DEFAULT_AMASK);
