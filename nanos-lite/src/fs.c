e2c05b16a87a5ba9e3ab07fad9e8d007986b97e9
tracer-ics2023 <tracer@njuics.org>
2023-10-31 11:52:25 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:52:25 up 1 day,  1:30,  1 user,  load average: 0.97, 0.87, 0.98

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,6 +63,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   
   for (int i = 0; i < NR_FILES; i ++) {
     if (strcmp(pathname, file_table[i].name) == 0) {
+      file_table[i].offset = 0;
       return i;
     }
   }
