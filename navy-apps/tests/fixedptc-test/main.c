a76689c864c1dc06d196cb58e149fb851c167bf9
tracer-ics2023 <tracer@njuics.org>
2023-11-07 15:19:33 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-87-generic #97-Ubuntu SMP Mon Oct 2 21:09:21 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:19:33 up 4 days, 13:08,  3 users,  load average: 0.65, 0.36, 0.41

--- navy-apps/tests/fixedptc-test/main.c
+++ navy-apps/tests/fixedptc-test/main.c
@@ -1,6 +1,25 @@
 #include <fixedptc.h>
 #include <stdio.h>
 
+fixedpt fixedpt_fromfloat(void *p) {
+    uint32_t o = *(uint32_t *)p;
+    if (o == 0U || o == (1U << 31)) {
+        return (fixedpt) 0U;
+    }
+    fixedpt r = 0;
+
+    uint32_t s, f, e;
+    s = (o & (1U << 31)) ? 1 : 0;
+    f = (o & ((1U << 23) - 1));
+    e = (o >> 23) & ((1U << 11) - 1);
+
+    int32_t t = f | (1U << 23);
+    if (s) {
+        t = ~t + 1;
+    }
+
+}
+
 int main() {
     printf(
         "[libfixedptc] Test: 123.456 * 456.789 = %s.\n",
