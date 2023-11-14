172210fbf523fcafb9eb61383d35066c1a0c0441
tracer-ics2023 <tracer@njuics.org>
2023-11-14 15:47:39 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:47:39 up  8:33,  0 users,  load average: 1.24, 1.32, 1.41

--- nemu/src/utils/snapshot.c
+++ nemu/src/utils/snapshot.c
@@ -9,9 +9,9 @@ bool snapshot_load(const char *filename) {
   if (!fp)
     goto error;
 
-  int rc = fread(&cpu, sizeof(cpu), 1, fp);
+  int rc = fread(&cpu, 1, sizeof(cpu), fp);
   if (rc != sizeof(cpu)) goto error;
-  rc = fread(guest_to_host(RESET_VECTOR), CONFIG_MSIZE, 1, fp);
+  rc = fread(guest_to_host(RESET_VECTOR), 1, CONFIG_MSIZE, fp);
   if (rc != CONFIG_MSIZE) goto error;
 
   difftest_sync();
@@ -27,9 +27,9 @@ bool snapshot_save(const char *filename) {
   if (!fp)
     goto error;
 
-  int rc = fwrite(&cpu, sizeof(cpu), 1, fp);
+  int rc = fwrite(&cpu, 1, sizeof(cpu), fp);
   if (rc != sizeof(cpu)) goto error;
-  rc = fwrite(guest_to_host(RESET_VECTOR), CONFIG_MSIZE, 1, fp);
+  rc = fwrite(guest_to_host(RESET_VECTOR), 1, CONFIG_MSIZE, fp);
   if (rc != CONFIG_MSIZE) goto error;
 
   return true;
