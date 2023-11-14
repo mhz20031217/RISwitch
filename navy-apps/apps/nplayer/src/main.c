92203aed1f4e99bdc721f5f8409343ea9d59b8ee
tracer-ics2023 <tracer@njuics.org>
2023-11-14 22:38:04 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:38:04 up 15:24,  1 user,  load average: 1.07, 1.71, 1.59

--- navy-apps/apps/nplayer/src/main.c
+++ navy-apps/apps/nplayer/src/main.c
@@ -7,7 +7,8 @@
 #include <vorbis.h>
 #include <fixedptc.h>
 
-#define MUSIC_PATH "/share/music/rhythm/Do.ogg"
+// #define MUSIC_PATH "/share/music/rhythm/Do.ogg"
+#define MUSIC_PATH "/share/music/moon-in-ones-cup.ogg"
 #define SAMPLES 4096
 #define FPS 10
 #define W 400
