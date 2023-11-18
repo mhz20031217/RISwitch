a42de60359806083fb05afd9387d0ec270eb7c51
tracer-ics2023 <tracer@njuics.org>
2023-11-18 22:17:38 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:17:38 up 4 days, 15:03,  2 users,  load average: 0.53, 0.61, 0.67

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -14,21 +14,40 @@ void hello_fun(void *arg) {
   int j = 1;
   while (1) {
     Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("The message is '%s'.", arg);
     j ++;
     yield();
   }
 }
 
+void context_kload(PCB *pcb, void (*func)(void *), void *arg) {
+  Area stack = { .start = pcb->stack, .end = pcb->stack + STACK_SIZE };
+  pcb->cp = kcontext(stack, func, arg);
+}
+
 void init_proc() {
+  Log("Initializing kthreads...");
+  context_kload(&pcb[0], hello_fun, "A is running.");
+  context_kload(&pcb[1], hello_fun, "B is running.");
+
+
   switch_boot_pcb();
 
   Log("Initializing processes...");
 
   // load program here
   void naive_uload(PCB *pcb, const char *filename);
-  naive_uload(NULL, "/bin/menu");
+  // naive_uload(NULL, "/bin/menu");
 }
 
 Context* schedule(Context *prev) {
-  return NULL;
+  current->cp = prev;
+  while (current->cp == prev || current->cp == NULL) {
+    if (current == pcb + MAX_NR_PROC - 1) {
+      current = pcb;
+    } else {
+      current += 1;
+    }
+  }
+  return current->cp;
 }
