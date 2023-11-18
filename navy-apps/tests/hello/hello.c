dc0b4b36d2ce8ecf78cbd280ed224fdff6da59d9
tracer-ics2023 <tracer@njuics.org>
2023-11-19 07:28:43 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:28:43 up 5 days, 14 min,  2 users,  load average: 0.91, 1.15, 0.90

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -9,6 +9,7 @@ int main() {
     j ++;
     if (j == 10000) {
       printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      printf("To ensure running on the user stack, address of j is %p.\n", &j);
       j = 0;
     }
   }
