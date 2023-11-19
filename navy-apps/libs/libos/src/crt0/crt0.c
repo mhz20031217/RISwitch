bf3af0d090bf59459ecf15c43b956ae44b2d5284
tracer-ics2023 <tracer@njuics.org>
2023-11-19 18:56:02 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:56:02 up 5 days, 11:42,  2 users,  load average: 1.18, 1.02, 1.07

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,10 +5,15 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 extern void __libc_init_array (void);
-void call_main(uintptr_t *args) {
+void call_main(void *args) {
   char *empty[] =  {NULL };
   environ = empty;
   __libc_init_array();
-  exit(main(0, empty, empty));
+  int argc = *(int *) args;
+  args += sizeof(int);
+  char **argv = *(char ***)args;
+  args += (argc + 1) * sizeof(char *);
+  char **envp = *(char ***)args;
+  exit(main(argc, argv, envp));
   assert(0);
 }
