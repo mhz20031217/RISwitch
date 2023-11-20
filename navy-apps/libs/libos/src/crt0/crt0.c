030e8bc1224538a8dc27ddef115ecace74ec326c
tracer-ics2023 <tracer@njuics.org>
2023-11-20 10:18:29 +0800

    >  compile NEMU 221830012 茆弘之 Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:18:29 up 6 days,  3:04,  2 users,  load average: 0.55, 0.61, 0.58

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
@@ -11,7 +13,31 @@ void call_main(void *args) {
   args += sizeof(int);
   char **argv = (char **)args;
   args += (argc + 1) * sizeof(char *);
+
+  // set environ
   char **envp = (char **)args;
+  char **eitem = envp;
+  for (; *eitem != NULL; eitem ++) {
+    char *name = strdup(*eitem);
+    char *value;
+    bool flag = false;
+    for (value = name; *value != '\0'; value ++) {
+      if (*value == '=') {
+        *value = '\0';
+        value ++;
+        while (*value == ' ') {
+          value ++;
+        }
+        break;
+      }
+    }
+
+    if (flag) {
+      setenv(name, value, 1);
+    }
+    
+    free(name);
+  }
   exit(main(argc, argv, envp));
   assert(0);
 }
