cbbde8a1f561f28a4b3141a49ca3fa736a86e305
221830012-Hongzhi Mao <hongzhimao@smail.nju.edu.cn>
2023-11-30 19:21:14 +0800

    [doc] Spec finished with AM Regs not explained.

--- abstract-machine/am/src/platform/switch/include/switch.h
+++ abstract-machine/am/src/platform/switch/include/switch.h
@@ -6,19 +6,19 @@
 
 # define switch_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
-#define SERIAL_PORT     0x8020000
-#define KBD_ADDR        0x8050000
-#define RTC_ADDR        0x8030000
-#define VGACTL_ADDR     0x0000000 // not supported yet
-#define AUDIO_ADDR      0x0000000 // not supported yet
-#define DISK_ADDR       0x0000000 // not supported yet
-#define FB_ADDR         0x0000000 // not supported yet
-#define AUDIO_SBUF_ADDR 0x0000000 // not supported yet
-#define CMEM_ADDR       0x8040000
-#define SW_ADDR         0x8060000
-#define BTN_ADDR        0x8070000
-#define LED_ADDR        0x8080000
-#define SEG_ADDR        0x8090000
+#define SERIAL_PORT     0x80200000
+#define KBD_ADDR        0x80500000
+#define RTC_ADDR        0x80300000
+#define VGACTL_ADDR     0x00000000 // not supported yet
+#define AUDIO_ADDR      0x00000000 // not supported yet
+#define DISK_ADDR       0x00000000 // not supported yet
+#define FB_ADDR         0x00000000 // not supported yet
+#define AUDIO_SBUF_ADDR 0x00000000 // not supported yet
+#define CMEM_ADDR       0x80400000
+#define SW_ADDR         0x80600000
+#define BTN_ADDR        0x80700000
+#define LED_ADDR        0x80800000
+#define SEG_ADDR        0x80900000
 
 extern char _pmem_start;
 #define PMEM_SIZE (256 * 1024)
