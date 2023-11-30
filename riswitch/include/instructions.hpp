2f5922cb0a9eb26e8f899a06daec34c1ee4a9d6f
茆弘之 <221830012@smail.nju.edu.cn>
2023-11-30 19:25:16 +0800

    Basic framework and documentation

---
+++ riswitch/include/instructions.hpp
@@ -0,0 +1,14 @@
+#ifndef _INSTRUCTIONS_HPP_
+#define _INSTRUCTIONS_HPP_
+
+#include <string>
+#include <vector>
+
+const std::vector<std::string> instructions = {
+    "add", "addi", "and", "andi", "auipc", "beq",  "bge", "bgeu",
+    "blt", "bltu", "bne", "jal",  "jalr",  "lb",   "lbu", "lh",
+    "lhu", "lui",  "lw",  "or",   "ori",   "sb",   "sh",  "simple",
+    "sll", "slli", "slt", "slti", "sltiu", "sltu", "sra", "srai",
+    "srl", "srli", "sub", "sw",   "xor",   "xori"};
+
+#endif // !_INSTRUCTIONS_HPP_
--- riswitch/scripts/nvdl_sim.mk
+++ riswitch/scripts/nvdl_sim.mk
@@ -5,33 +5,39 @@ include scripts/nvdl.mk
 
 BIN = V$(SIM_TOP)_sim
 WAVE = $(BUILD_DIR)/$(SIM_TOP).vcd
+WAVECFG = $(TEST_DIR)/$(SIM_TOP).gtkw
+SIM_TIMESTAMP = $(BUILD_DIR)/.sim.$(SIM_TOP).timestamp
 .DEFAULT_GOAL = sim
 
 VERILATOR_FLAGS += -DSIM --trace --top $(SIM_TOP) --public
 CSRCS += $(shell find $(abspath $(TEST_DIR)) $(CSRC_PATTERN))
 VSRCS += $(shell find $(abspath $(TEST_DIR)) $(VSRC_PATTERN))
+HEADERS += $(shell find $(abspath $(TEST_DIR)) $(HEADER_PATTERN))
 
 CFLAGS += $(addprefix -I,$(abspath $(INCLUDE_PATH)))
 
 .PHONY: wave
 wave: $(WAVE)
 	@echo "### WAVEFORM ###"
-	@$(GTKWAVE) $(WAVE)
+	@$(GTKWAVE) --save=$(WAVECFG) --saveonexit $(WAVE)
 
-$(WAVE):sim
+$(WAVE): $(SIM_TIMESTAMP)
 
 .PHONY: sim
-sim: compile
+sim: $(SIM_TIMESTAMP)
+
+$(SIM_TIMESTAMP): $(BUILD_DIR)/$(BIN)
 	$(call git_commit, "sim RTL")
 	@echo "### SIMULATION ###"
 	@cd $(BUILD_DIR) && ./$(BIN)
+	@touch $(SIM_TIMESTAMP)
 
-$(BUILD_DIR)/$(BIN): $(CSRCS) $(VSRCS)
+$(BUILD_DIR)/$(BIN): $(CSRCS) $(VSRCS) $(HEADERS)
+	@echo "### COMPILATION ###"
 	$(VERILATOR) $(VERILATOR_FLAGS) \
 		$(addprefix -CFLAGS , $(CFLAGS)) $(addprefix -LDFLAGS , $(LDFLAGS)) \
 		$(VSRCS) $(CSRCS) -o $(BIN)
 
 .PHONY: compile
 compile: $(BUILD_DIR)/$(BIN)
-	@echo "### COMPILATION ###"
 
