cfa2ceee5a32011cb0ca955f372a8fbc5d0b62cd
tracer-ics2023 <tracer@njuics.org>
2023-11-30 11:50:47 +0800

    >  sim RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:50:47 up 1 day, 56 min,  2 users,  load average: 0.69, 0.77, 0.76

--- riswitch/tests/nvdl_batch/nvdl_batch.cpp
+++ riswitch/tests/nvdl_batch/nvdl_batch.cpp
@@ -1,12 +1,12 @@
 #include <VSystem.h>
-#include <cstdlib>
-#include <iomanip>
-#include <verilated.h>
 #include <common.hpp>
+#include <cstdlib>
+#include <instructions.hpp>
+#include <iomanip>
 #include <memory.hpp>
 #include <unistd.h>
+#include <verilated.h>
 #include <verilated_vcd_c.h>
-#include <instructions.hpp>
 
 VSystem *dut;
 VerilatedVcdC *tracer;
@@ -26,7 +26,8 @@ static uint64_t get_time_internal() {
 }
 
 static uint64_t get_time() {
-  if (boot_time == 0) boot_time = get_time_internal();
+  if (boot_time == 0)
+    boot_time = get_time_internal();
   uint64_t now = get_time_internal();
   return now - boot_time;
 }
@@ -43,12 +44,9 @@ static void nvdl_init(int argc, char **argv) {
   dut->clock = 0; // starting on rising edge
   sim_time = 0;
   clk_cnt = 0;
-
 }
 
-static void nvdl_destroy() {
-  delete dut;
-}
+static void nvdl_destroy() { delete dut; }
 
 static void nvdl_loop_begin() {
 #ifdef CLK_RT
@@ -69,10 +67,12 @@ static void nvdl_loop_end() {
   dut->eval();
 #ifdef CLK_RT
   // ensure that 100000 cycles per 10ms (10^8Hz = 10MHz)
-  // uncomment the printf statement to check whether the simulation is running fast enough
+  // uncomment the printf statement to check whether the simulation is running
+  // fast enough
   uint64_t now;
   if (clk_cnt == 99999) {
-    while ((now = get_time()) < sim_time + 10000000);
+    while ((now = get_time()) < sim_time + 10000000)
+      ;
     // printf("End  : %lu\n", now);
     clk_cnt = 0;
   } else {
@@ -83,17 +83,17 @@ static void nvdl_loop_end() {
   tracer->dump(sim_time);
 }
 
-bool check_status(const std::string& name) {
+bool check_status(const std::string &name) {
   if (!dut->halt) {
     return false;
   }
-  for (int i = 0; i < 5; i ++) {
+  for (int i = 0; i < 5; i++) {
     nvdl_loop_begin();
     nvdl_loop_end();
-  } 
-  
+  }
+
   if (dut->trap) {
-    std::cout << "[" << name << "]\tHit GOOD trap.\n"; 
+    std::cout << "[" << name << "]\tHit GOOD trap.\n";
     return true;
   } else {
     std::cout << "[" << name << "]\tHit BAD trap.\n";
@@ -106,8 +106,9 @@ void run_test(const std::string name) {
   // load memory
   imem_load(("../tests/cpu_pipebatch/rv32ui-p-" + name + ".hex").c_str());
   dmem_load(("../tests/cpu_pipebatch/rv32ui-p-" + name + "_d.hex").c_str());
-  
-  while (sim_time - test_start_time < max_test_time && sim_time < max_sim_time) {
+
+  while (sim_time - test_start_time < max_test_time &&
+         sim_time < max_sim_time) {
     // std::cerr << "Sim time: " << sim_time << '\n';
     if (sim_time - test_start_time < 9) {
       dut->reset = 1;
@@ -115,7 +116,7 @@ void run_test(const std::string name) {
       dut->reset = 0;
     }
     nvdl_loop_begin();
-//    printf("sim_time: %ld, pc: %x.\n", sim_time, dut->pc);
+    //    printf("sim_time: %ld, pc: %x.\n", sim_time, dut->pc);
     nvdl_loop_end();
     if (check_status(name)) {
       return;
@@ -129,10 +130,9 @@ int main(int argc, char *argv[], char *envp[]) {
   nvdl_init(argc, argv);
   std::atexit(nvdl_destroy);
 
-  for (auto name: instructions) {
+  for (auto name : instructions) {
     run_test(name);
   }
 
   return 0;
 }
-
