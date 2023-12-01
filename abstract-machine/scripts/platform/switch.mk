d383d4bcbd865f01780f34ba0140f76df898f12b
221830012-Hongzhi Mao <hongzhimao@smail.nju.edu.cn>
2023-12-01 11:15:03 +0800

    [framework] AM Makefile generates and passes images to RISwitch Makefile.

--- abstract-machine/scripts/platform/switch.mk
+++ abstract-machine/scripts/platform/switch.mk
@@ -17,13 +17,22 @@ CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/switch/include
 .PHONY: $(AM_HOME)/am/src/riscv/switch/trm.c
 
-IMEM_IMG = $(IMAGE).hex
-DMEM_IMG = $(IMAGE)_d.hex
+IMEM_IMG = $(IMAGE_REL).hex
+DMEM_IMG = $(IMAGE_REL)_d.hex
+
+PYTHON = python3
+HEXGEN = $(AM_HOME)/tools/switch-img.py
 
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
-	@$(OBJCOPY) -S --set-section-flags .bss=noload -O binary $(IMAGE).elf $(IMAGE).bin
-
-run: image
-	$(MAKE) -C $(SWITCH_HOME) IMEM_IMG="$(IMEM_IMG)" DMEM_IMG="$(DMEM_IMG)" PLATFORM=NVDL MODE=EVAL
+	@$(OBJCOPY) -S --only-section=.text* -O binary $(IMAGE).elf $(IMAGE).bin
+	@echo + OBJCOPY "->" $(IMAGE_REL)_d.bin
+	@$(OBJCOPY) -S --only-section=*data* --only-section=*bss* --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE)_d.bin
+	@echo + HEXGEN "->" $(IMAGE_REL).hex \(InstrMem Image\)
+	@$(PYTHON) $(HEXGEN) $(IMAGE_REL).bin $(IMEM_IMG)
+	@echo + HEXGEN "->" $(IMAGE_REL)_d.hex \(DataMem Image\)
+	@$(PYTHON) $(HEXGEN) $(IMAGE_REL)_d.bin $(DMEM_IMG)
+	
+sim: image
+	$(MAKE) -C $(SWITCH_HOME) IMEM_IMG="$(abspath $(IMEM_IMG))" DMEM_IMG="$(abspath $(DMEM_IMG))" PLATFORM=NVDL MODE=SIM
--- abstract-machine/tools/switch-img.py
+++ abstract-machine/tools/switch-img.py
@@ -9,13 +9,14 @@ assert bin != ''
 assert img_hex != ''
 
 fp_img = open(img_hex, 'w')
+fp_img.write('@00000000\n')
 
 with open(bin, 'rb') as fp:
     while True:
         bytes = fp.read(4)
         if not bytes:
             break
-        fp_img.write(bytes[::-1].hex() + ' ')
+        fp_img.write(bytes[::-1].hex() + '\n')
     fp.close()
 
 fp_img.close()
