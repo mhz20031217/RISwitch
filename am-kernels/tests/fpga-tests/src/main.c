8ae5c750adb0d5f2f4e8dde4061b82035caef06e
tracer-ics2023 <tracer@njuics.org>
2023-12-05 15:54:32 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:54:32 up 4 days, 16:43,  3 users,  load average: 0.38, 0.38, 0.42

--- am-kernels/tests/fpga-tests/src/main.c
+++ am-kernels/tests/fpga-tests/src/main.c
@@ -9,7 +9,7 @@ void delay() {
 void led_test() {
   for (uint16_t i = 1; i; i <<= 1) {
     io_write(AM_LED, i);
-    delay();
+    //delay();
   }
 }
 
@@ -17,8 +17,8 @@ void seg_test() {
   uint32_t v = 0xabcdef88;
   for (int i = 0; i < 20; i ++) {
     io_write(AM_SEG, v);
-    delay();
-    i = (i << 4) | (i >> 28);
+    //delay();
+    v = (v << 4) | (v >> 28);
   }
 }
 
