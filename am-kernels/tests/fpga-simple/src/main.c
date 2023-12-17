f6c19025e064c2ffd2ae7d963593a64204190f72
tracer-ics2023 <tracer@njuics.org>
2023-12-17 16:47:29 +0800

    >  sim RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:47:29 up 3 days,  7:08,  2 users,  load average: 0.84, 0.73, 0.62

---
+++ am-kernels/tests/fpga-simple/src/main.c
@@ -0,0 +1,28 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+
+void delay() {
+  for (volatile int i = 0; i < 10; i ++);
+}
+
+void led_test() {
+  for (volatile uint16_t i = 1; i; i <<= 1) {
+    io_write(AM_LED, i);
+    //delay();
+  }
+  io_write(AM_LED, 1234);
+}
+
+void seg_test() {
+  volatile uint32_t v = 0xabcdef88;
+  for (volatile int i = 0; i < 1000000; i ++) {
+    io_write(AM_SEG, v);
+    delay();
+    v = (v << 4) | (v >> 28);
+  }
+}
+
+int main(const char *args) {
+  halt(SWITCH_EXIT_SUCCESS);
+}
