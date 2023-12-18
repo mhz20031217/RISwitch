88089a43b12f22f53d440390888412ff9d0f26fd
tracer-ics2023 <tracer@njuics.org>
2023-12-18 17:45:04 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:45:04 up 4 days,  8:06,  2 users,  load average: 0.45, 0.50, 0.52

--- riswitch/vsrc/Timer.sv
+++ riswitch/vsrc/Timer.sv
@@ -3,6 +3,9 @@ import "DPI-C" function void timer_read(input int is_high, output int data);
 `endif
 
 module Timer (
+  `ifdef VIVADO
+  input CLK_INPUT,
+  `endif
   input clock,
   input reset,
   input sel,
@@ -15,6 +18,32 @@ always @(posedge clock) begin
   if (sel) begin
     timer_read({31'b0, addr[2]}, dout);
     $display("[verilog timer] read %x", dout);
+  end
+end
+`elsif VIVADO
+reg [63:0] uptime;
+
+wire clk_1mhz;
+clkgen #(100000000, 1000000) clk_1mhz_gen(
+  .in(CLK_INPUT),
+  .out(clk_1mhz)
+);
+
+always @(posedge clk_1mhz) begin
+  if (reset) begin
+    uptime <= 64'b0;
+  end else begin
+    uptime <= uptime + 1;
+  end
+end
+
+always @(posedge clock) begin
+  if (sel) begin
+    if (addr[2]) begin
+      dout <= uptime[63:32];
+    end else begin
+      dout <= uptime[31:0];
+    end
   end
 end
 `endif
