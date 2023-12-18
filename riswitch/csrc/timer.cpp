b327c393a31e241d4dfc597169246a7558972f93
tracer-ics2023 <tracer@njuics.org>
2023-12-18 13:05:10 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:05:10 up 4 days,  3:26,  2 users,  load average: 0.61, 0.51, 0.59

--- riswitch/csrc/timer.cpp
+++ riswitch/csrc/timer.cpp
@@ -1,14 +1,12 @@
 #include <common.hpp>
 
-static uint64_t us;
-
 static uint64_t boot_time = 0;
 
 static uint64_t get_time_internal() {
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
-  uint64_t ns = now.tv_sec * 1000000 + now.tv_nsec/1000;
-  return ns;
+  uint64_t us = now.tv_sec * 1000000 + now.tv_nsec/1000;
+  return us;
 }
 
 uint64_t get_time() {
