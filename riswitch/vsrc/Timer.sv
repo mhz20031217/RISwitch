a20a77b05f37516683462accd3fbe5f71d65f9a0
tracer-ics2023 <tracer@njuics.org>
2023-12-18 17:21:43 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:21:43 up 4 days,  7:43,  2 users,  load average: 0.40, 0.27, 0.28

--- riswitch/vsrc/Timer.sv
+++ riswitch/vsrc/Timer.sv
@@ -13,8 +13,8 @@ module Timer (
 `ifdef NVDL
 always @(posedge clock) begin
   if (sel) begin
-    $display("[verilog timer] read");
     timer_read({31'b0, addr[2]}, dout);
+    $display("[verilog timer] read %d", dout);
   end
 end
 `endif
