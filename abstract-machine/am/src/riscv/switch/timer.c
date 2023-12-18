835c462fad92203844d36959ae7e35603d67ad63
tracer-ics2023 <tracer@njuics.org>
2023-12-18 16:39:25 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:39:25 up 4 days,  7:00,  2 users,  load average: 0.61, 0.87, 0.71

--- abstract-machine/am/src/riscv/switch/timer.c
+++ abstract-machine/am/src/riscv/switch/timer.c
@@ -6,6 +6,7 @@ static uint64_t boot_time = 0;
 
 static inline uint64_t get_time() {
   volatile uint64_t now = inl(RTC_ADDR);
+  printf("%lu ", now);
   return (now | ((volatile uint64_t)inl(RTC_ADDR + 4)) << 32);
 }
 
