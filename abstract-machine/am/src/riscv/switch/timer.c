6d52a0fd9a7a8d03468fd6e9244b7aeb97ee37d6
tracer-ics2023 <tracer@njuics.org>
2023-12-18 16:29:38 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:29:38 up 4 days,  6:51,  2 users,  load average: 0.58, 0.63, 0.57

--- abstract-machine/am/src/riscv/switch/timer.c
+++ abstract-machine/am/src/riscv/switch/timer.c
@@ -1,19 +1,22 @@
 #include <am.h>
+#include <klib.h>
 #include <switch.h>
 
 static uint64_t boot_time = 0;
 
 static inline uint64_t get_time() {
   uint64_t now = inl(RTC_ADDR);
-  return (now | (uint64_t)inl(RTC_ADDR + 4) << 32);
+  return (now | ((uint64_t)inl(RTC_ADDR + 4)) << 32);
 }
 
 void __am_timer_init() {
   boot_time = get_time();
+  printf("[am-timer] Init, boot_time = %ld.\n", boot_time);
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = get_time() - boot_time;
+  printf("[am-timer] update: %ld.\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
