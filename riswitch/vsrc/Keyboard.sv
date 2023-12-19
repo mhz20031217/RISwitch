1e3b4d13175deb61f405d94b4abad37ba1d6f245
tracer-ics2023 <tracer@njuics.org>
2023-12-19 15:49:10 +0800

    >  eval RTL Linux ubuntu-VirtualBox 6.2.0-37-generic #38~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov  2 18:01:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:10 up 13:32,  1 user,  load average: 0.46, 0.31, 0.27

--- riswitch/vsrc/Keyboard.sv
+++ riswitch/vsrc/Keyboard.sv
@@ -31,7 +31,7 @@ always@(posedge clk) begin
 
         if(en == 0) begin
             //cur_key <= cur_key;
-            cur_key <= 32'h0000001c;
+            cur_key <= 32'h0000F01c;
             nextdata_n <= 1;
         end
         else if(nextdata_n == 0) nextdata_n <= 1;
