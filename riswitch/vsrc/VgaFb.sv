9af54fafedd0f259af668858fd64fcc2d1db4a7f
tracer-ics2023 <tracer@njuics.org>
2023-12-20 17:53:48 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:53:48 up 1 day,  3:36,  2 users,  load average: 0.82, 0.62, 0.66

---
+++ riswitch/vsrc/VgaFb.sv
@@ -0,0 +1,27 @@
+`timescale 10ns/1ns
+module VgaFb (
+  input clock, reset,
+  input sel, we,
+  input [9:0] h_addr, v_addr,
+  input [31:0] addr,
+  input [31:0] din,
+  output reg [11:0] vga_data
+);
+
+`ifdef NVDL
+reg [15:0] mem [327679:0];
+
+wire [18:0] addr_r = {h_addr, v_addr[8:0]};
+
+always @(posedge clock) begin
+  if (sel & we) begin
+    mem[addr[19:1]] <= din;
+  end
+  vga_data <= mem[addr_r];
+end
+
+`elsif VIVADO
+
+`endif
+    
+endmodule
