5ef01bbbf3c98714acc8abcb85ba9666ebdccbd6
tracer-ics2023 <tracer@njuics.org>
2023-12-28 11:28:39 +0800

    >  eval RTL Linux pc 5.15.0-88-generic #98-Ubuntu SMP Mon Oct 2 15:18:56 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:28:39 up 21:47,  2 users,  load average: 1.34, 0.90, 0.44

--- riswitch/include/memory.hpp
+++ riswitch/include/memory.hpp
@@ -4,7 +4,7 @@
 #include <common.hpp>
 
 #define IMEM_SIZE 131072U // 128KB
-#define DMEM_SIZE 131072U * 8 // 2048KB
+#define DMEM_SIZE 131072U * 16 // 2048KB
 
 void imem_load(const char *filename);
 
