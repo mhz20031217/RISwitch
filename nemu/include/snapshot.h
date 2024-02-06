accfb66d1cdca3f4806aca9051c580b5a74359a0
Caterpillar <mhz20031217@126.com>
2024-02-06 23:17:23 +0800

    Delete nemu directory

--- nemu/include/snapshot.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SNAPSHOT_H__
-#define __SNAPSHOT_H__
-
-#include <common.h>
-
-bool snapshot_load(const char *filename);
-bool snapshot_save(const char *filename);
-
-#endif // __SNAPSHOT_H__
